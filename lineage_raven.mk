56b828c8ed6f41d21f17861cc435e46bdda82d75
opensourcefreak <john@snurr.nu>
2024-01-03 18:57:00 +0100

    Alpha bringup

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -29,4 +29,44 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/raven/raven:13/TQ3A.230901.001/10750268:user/release-keys
 
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GAPPS := false
+TARGET_INCLUDE_GOOGLE_TELEPHONY := false
+TARGET_INCLUDE_PIXEL_FRAMEWORK := false
+TARGET_INCLUDE_GOOGLE_CAMERA := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_INCLUDE_GMAIL := false
+TARGET_INCLUDE_GOOGLE_MAPS := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unoficcial
+ALPHA_MAINTAINER := opensourcefreak
+
+# Artifact path requirement fix
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST +=                               \
+system/etc/permissions/android.hardware.biometrics.face.xml             \
+system/etc/permissions/privapp-permissions-com.crdroid.faceunlock.xml   \
+system/etc/sysconfig/hiddenapi-whitelist-com.crdroid.faceunlock.xml     \
+system/lib64/faceunlock_vendor_dependencies.so                          \
+system/lib64/libFaceDetectCA.so                                         \
+system/lib64/libMegviiUnlock-jni-1.2.so                                 \
+system/lib64/libMegviiUnlock.so                                         \
+system/lib64/libarcsoft-lib.so                                          \
+system/lib64/libarcsoft_faceid.so                                       \
+system/lib64/libarcsoftbase.so                                          \
+system/lib64/libmegface.so                                              \
+system/lib64/libmpbase.so                                               \
+system/priv-app/FaceUnlockService/FaceUnlockService.apk
+
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
